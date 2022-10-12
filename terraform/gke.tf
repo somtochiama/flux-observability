f786c4a568ecf83a342052b8d8009411f8e2a13c
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2022-10-12 13:05:57 +0100

    createe pvx
    Signed-off-by: Somtochi Onyekwere <somtochionyekwere@gmail.com>

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -18,7 +18,7 @@ resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
   location   = data.google_client_config.this.region 
   cluster    = google_container_cluster.primary.name
-  node_count = 2
+  node_count = 3
 
   node_config {
     preemptible  = true
